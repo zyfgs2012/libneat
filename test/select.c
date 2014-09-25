76a83ea165ad14a3b559527f2fb974bf2ff19ffb
zyfgs2012 <zyfgs2012@163.com>
2014-09-25 17:52:23 +0800

    select sample

---
+++ test/select.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char buffer[128];
+	int res,nread;
+	fd_set inputs,test_fds;
+	struct timeval timeout;
+	FD_ZERO(&inputs);
+	FD_SET(0,&inputs);
+
+	while(1){
+		test_fds = inputs;
+		timeout.tv_sec = 2;
+		timeout.tv_usec = 500000;
+		res = select(FD_SETSIZE,&test_fds,NULL,NULL,NULL);
+		switch (res) {
+			case 0:
+				printf("Timeout\n");
+				break;
+			case -1:
+				perror("Error in select\n");
+				exit(1);
+			default:
+				if(FD_ISSET(0,&test_fds)) {
+					ioctl(0,FIONREAD,&nread);
+					if(0 == nread) {
+						printf("input end!\n");
+						exit(0);
+					}
+					nread = read(0,buffer,nread);
+					printf("read %d from keyboard: %s",nread,buffer);
+				}
+				break;
+		}
+	}
+	return 0;
+}
+
